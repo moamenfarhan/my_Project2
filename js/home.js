203b5155877fb9d74eedbf1b31ee8cced69b8ed0
moamenfarhan <129553479+moamenfarhan@users.noreply.github.com>
2023-04-01 23:53:07 +0300

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,16 @@
+document.getElementById("aa").style.display="none";
+
+
+
+function moamen(){
+    document.getElementById("img").setAttribute("src","/images/Clinic1.png");
+    document.getElementById("aa").style.display="block"
+}
+function moamen1(){
+    document.getElementById("img").setAttribute("src","/images/Clinic2.png");
+    document.getElementById("aa").style.display="block"
+}
+function moamen2(){
+    document.getElementById("aa").style.display="none"
+}
+
